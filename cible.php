69ad5bb145b0880175cbef7ca596c2042c74ab53
Thomas-381 <83665898+Thomas-381@users.noreply.github.com>
2024-04-09 19:18:31 +0200

    Add .php file

---
+++ cible.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=”utf-8”>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>Introduction à PHP</title>
+	</head>
+
+	<body>
+
+		<p> 
+			Bonjour !
+		</p>
+		
+		<p> Tu t'appelles <?php echo $_POST['prenom']; ?> ! </p>
+		
+		<p> Si tu veux changer de prénom, <a href="formulaire.php"> Clique ici </a> pour revenir à
+		page de formulaire.php. </p>
+		
+		
+	</body>
+	
+	
+</html>
---
+++ formulaire.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=”utf-8”>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>Introduction à PHP</title>
+	</head>
+
+	<body>
+
+		<p> 
+			Cette page ne contient que du code html. <br>
+			Veuillez taper votre nom :
+		</p>
+		
+		<form action = "cible.php" method = "post">
+		
+		<p>
+			<input type = "text" name ="prenom"/>
+			<input type = "submit" value ="Valider"/>
+			
+		</p>
+		</form>
+
+
+
+	</body>
+	
+	
+</html>
